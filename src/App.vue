228864c6a7fd645302cf5a5b3a0691bef5c0db32
steven.roulleau <steven.roulleau@myscript.com>
2019-06-19 13:58:25 +0200

    feat(comp): add calculator with Rust

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Calculator :a="1" :b="1">+</Calculator>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import loadWasm from './lib.rs'
+import Calculator from './components/Calculator.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Calculator
+  },
+  beforeCreate () {
+    loadWasm()
+      .then(async (result) => {
+        const pointer = result.instance.exports.get_key()
+        const stringBuffer = new Uint8Array(result.instance.exports.memory.buffer, pointer, 9)
+        console.log(this.fromBuffer(stringBuffer))
+        result.instance.exports.free_key(pointer)
+      })
+      .catch(err => console.error(err))
+  },
+  methods: {
+    fromBuffer (buff) {
+      let key = ''
+      for (let i = 0; i < buff.length; i++) {
+        key += String.fromCharCode(buff[i])
+      }
+      return key
+    }
   }
 }
 </script>
